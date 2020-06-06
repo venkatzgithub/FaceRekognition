b15fc3ad208af26e316daa9f8cb033b36270152a
venkatzgithub <imvenkatprasad@gmail.com>
2020-06-06 18:12:09 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
 import './App.css';
+import ImageForm from "./components/ImageForm/ImageForm";
+import Rank from "./components/Rank/Rank";
+import Particles from 'react-particles-js';
+import {Component} from "react";
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+const app = new Clarifai.App({
+ apiKey: '2e14f2c6ed92446a88d69c2bd99dbd59'
+});
+const particlestoptions= {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 500
+      }
+    }
+  }
+}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
+    }
+  }
+  onInputChange = (event) => {
+    this.setState({input: event.target.value}, () => console.log(this.state.input));
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  onButtonSubmit=()=>{
+    this.setState({imageUrl: this.state.input});
+
+    console.log(this.state.imageUrl);
+    app.models.predict(
+Clarifai.COLOR_MODEL,
+    // URL
+    "https://samples.clarifai.com/metro-north.jpg"
+)
+.then(function(response) {
+console.log(response);
+    });
+  }
+
+  render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
+    return (
+      <div className="App">
+      <Particles className="particles"
+            params={particlestoptions}
+          
+          />
+      <Navigation></Navigation>
+      <Logo></Logo>
+      <Rank></Rank>
+      <ImageForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}  ></ImageForm>
+      <FaceRecognition imageUrl={imageUrl}></FaceRecognition>
+  </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const FaceRecognition=({imageUrl})=>
+{
+    //console.log(imageUrl);
+return ( <div>
+    <img src={imageUrl} width='500px' height='auto' alt=""></img>
+  </div>)
+}
+export default FaceRecognition;
